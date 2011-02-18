e00bafca2f56902cdad7df6ce89a1cdf66bee6b6
Sina K. Heshmati <sina@khakbaz.com>
2011-02-18 13:16:51 +0100

    Adds new abstractions for parsing class fields and methods.

---
+++ src/objective-racket/check.rkt
@@ -0,0 +1,71 @@
+#lang racket
+
+(require "utils.rkt")
+(provide
+ public-class-method?
+ public-class-field?)
+
+(define (handler exn) #f)
+(define-syntax (check stx)
+  (syntax-case stx ()
+    ((check member conditions ...)
+     #'(begin
+         (set! member (syntax->datum member))
+         (with-handlers ((exn:fail? handler))
+           (and conditions ...))))))
+
+(define (get-access-modifier member)
+  (car member))
+
+(define (get-scope member)
+  (cadr member))
+(define (after-scope member)
+  (cddr member))
+  
+(define (public? member)
+  (eq? (get-access-modifier member) 'public))
+
+(define (private? member)
+  (eq? (get-access-modifier member) 'private))
+
+(define (static? member)
+  (eq? (get-scope member) 'static))
+
+(define (field-definition? exp)
+  (and 
+   (not (null? (car exp)))
+   (not (null? (cadr exp)))))
+
+(define (method-definition? exp)
+  (and
+   (not (null? (car exp)))
+   (not (null? (caddr exp)))))
+
+(define (public-class-field? member)
+  (check 
+   member
+   (public? member)
+   (static? member)
+   (field-definition? 
+    (after-scope member))))
+
+(define (public-class-method? member)
+  (check
+   member
+   (public? member)
+   (static? member)
+   (method-definition? 
+    (after-scope member))))
+
+(define (private-class-method? member)
+  (check 
+   member
+   (private? member)
+   (static? member)
+   (method-definition?
+    (after-scope member))))
+
+;(public-class-field? #'(public static nr-of 0))
+;(public-class-method? #'(public static nr-of 0))
+;(public-class-method? #'(public static go () x))
+;(private-class-method? #'(private static go (x) x))
--- src/objective-racket/main.rkt
+++ src/objective-racket/main.rkt
@@ -4,20 +4,9 @@
  (for-syntax racket)
  (for-syntax "utils.rkt")
  (for-syntax "table.rkt")
+ (for-syntax "check.rkt")
  "utils.rkt"
  "table.rkt")
-
-(define-for-syntax (member-matches? pattern member)
-  (match (syntax->datum member)
-    (pattern #t)))
-
-(define-for-syntax (public-class-field? member)
-  (match (syntax->datum member)
-    ((list public static var-name var-value) #t)))
-  
-(define-for-syntax (public-class-method? member)
-  (match (syntax->datum member)
-    ((list public static method-name method-params method-body) #t)))
 
 (define-for-syntax (process-members stx members)
   (deftable members-db)
@@ -33,33 +22,46 @@
   (for-each
    (λ (member)
      (when (public-class-method? member)
-       (members-db-add+ 'public-class-method member)))
+       (members-db-add+ 'public-class-methods member)))
    members)
-  
-  (let ((public-class-fields
-         (members-db-map+ 
-          'public-class-fields 
-          (λ (member)
-            (syntax-case member (static)
-              ((public static var-name var-value)
-               #'(define var-name var-value)))))))
     
-    (show public-class-fields)
+  (with-syntax
+      ((public-class-fields
+        #`(begin
+            #,@(members-db-map+
+                'public-class-fields
+                (λ (member)
+                  (syntax-case member (static)
+                    ((public static var-name var-value)
+                     #'(define var-name var-value)))))))
+       (public-class-methods
+        (car (members-db-map+
+                'public-class-methods
+                (λ (member)
+                  (syntax-case member (public static)
+                    ((public static method-name (method-param ...)
+                             method-body ...)
+                     #'(define method-name
+                         (λ (method-param ...)
+                           method-body ...))))))))
+       )
     #`(begin
-        #,@public-class-fields
-        #''done)))
-  
+        public-class-fields
+        public-class-methods
+        'ok)))
+
 (define-syntax (defclass stx)
   (syntax-case stx ()
     ((defclass name parent . members)
      #`(define (name)
-         (define meta '())
+         (define meta 'meta)
          #,(process-members stx (syntax->list #'members))))))
 
 (defclass NCard Object
   
   ;; Public (Static) Fields
   ; (public name)
+  
   (public static number-of-cards 0)
   (public static cool-meta 0)
   
@@ -70,14 +72,16 @@
 ;  ;; Public (Static) Methods 
 ;  (public play ()
 ;          (show "Play the card"))
-  (public static all-names-as-list ()
+  
+  (public static all-names-as-list (x)
+          (+ 2 3)
           (show "Put the name of all cards in a list"))
-;  
-;  ;; Private (Static) Methods 
-;  (private check ()
-;           (show "Checks stuff"))
-;  (private static all-statuses-as-list ()
-;           (show "Put the status of all cards in a list"))
+  
+  ;; Private (Static) Methods 
+  ;(private check ()
+  ;         (show "Checks stuff"))
+  ;(private static all-statuses-as-list ()
+  ;         (show "Put the status of all cards in a list"))
   )
 
 (NCard)
