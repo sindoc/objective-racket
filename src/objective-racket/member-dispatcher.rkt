2a29404349d162dd2c0592827827b0e9505af663
Sina K. Heshmati <sina@khakbaz.com>
2011-02-27 23:15:02 +0100

    Removes unnecessary dependencies and test code

--- src/objective-racket/member-dispatcher.rkt
+++ src/objective-racket/member-dispatcher.rkt
@@ -3,9 +3,7 @@
 (require 
  (for-template racket)
  (for-syntax "members.rkt")
- (for-syntax "utils.rkt")
- "members.rkt"
- "utils.rkt")
+ "members.rkt")
 
 (provide qualify-member)
 
@@ -25,21 +23,3 @@
                     (#,qualifier member)))
                *member-qualifiers*
                *member-qualifier-matchers*))))))
-
-;(define test-1
-;  (qualify-member 
-;   #'(public static field a 1)))
-;
-;(define test-2
-;  (qualify-member 
-;   #'(public 
-;       static 
-;       method
-;       b (x)
-;       (define y (+ x y))
-;       y)))
-;
-;(define test-3
-;  (qualify-member #'(private static field c 10)))
-;
-;(list test-1 test-2 test-3)
