b00c050ddc743742ff1c12149364f4a08cba3bb2
Sina K. Heshmati <sina@khakbaz.com>
2011-02-27 23:31:27 +0100

    [refactoring] Splits members.rkt into members.rkt and member-collector.rkt
    Now, members.rkt can contain pure semantics.

--- src/objective-racket/members.rkt
+++ src/objective-racket/members.rkt
@@ -1,66 +1,12 @@
 #lang racket
 
 (require
- (for-syntax "utils/main.rkt")
- (for-syntax racket)
- (for-template racket))
+ "member-collector.rkt")
 
-(provide 
- (all-defined-out)
- (for-syntax
-  (all-defined-out)))
-
-(define-for-syntax *member-qualifiers* null)
-(define-for-syntax *member-qualifier-matchers* null)
-(define-for-syntax *member-keywords* null)
-
-(define-for-syntax *ldots* '~ldots)
-
-(define-for-syntax (replace-specials stx)
-  (map
-   (λ (x)
-     (case x
-       ((*ldots*) '...)
-       (else x)))
-   (syntax->datum stx)))
-  
-(define-for-syntax (add-qualifier! id matcher keywords)
-  (set! *member-qualifiers* 
-        (cons id *member-qualifiers*))
-  (set! *member-qualifier-matchers*
-        (cons (replace-specials (cadr (syntax->list matcher)))
-              *member-qualifier-matchers*))
-  (set! *member-keywords*
-        (remove-duplicates
-         (append 
-          (syntax->datum (cadr (syntax->list keywords)))
-          *member-keywords*))))
-
-(define-syntax (def-member-qualifier stx)
-  (syntax-case stx ()
-    ((_ id (matcher matcher-expr keywords) (action-id action) ...)
-     (with-syntax
-         ((qualifier 
-           (make-id #'id "~a" #'id))
-          (qualifier-dispatcher 
-           (make-id #'id "dispatch-~a" #'id))
-          (qualifier-name #''id)
-          (qualifier-matcher
-           (make-id #'id "~a-matcher" #'id)))
-       #'(begin
-           (define-syntax _
-             (add-qualifier! #'id #'matcher-expr #'keywords))
-           (define (qualifier member)
-             (define (qualifier-dispatcher msg . args)
-               (case msg
-                 ((name) qualifier-name)
-                 ((member) member)
-                 ((action-id) 
-                  (apply action (list member))) ...
-                 (else
-                  (error qualifier-name "unknown message (~a) to ~a"
-                         msg "to class member qualifier"))))
-             qualifier-dispatcher))))))
+(provide
+ (for-syntax *member-qualifiers*)
+ (for-syntax *member-qualifier-matchers*)
+ (for-syntax *member-keywords*))
 
 (define (common-field-binder member)
   (let* ((as-list (syntax->datum member))
