6ccb99ff43490bd0409ef3873d9484ef3c8f3539
Sina K. Heshmati <sina@khakbaz.com>
2011-02-28 03:24:16 +0100

    Tests whether methods can access previously defined fields

--- src/objective-racket/tests/class-account.rkt
+++ src/objective-racket/tests/class-account.rkt
@@ -5,10 +5,11 @@
 
 (defclass Account Object
   (public static field n-accounts 0)
-  (private static field total-funds 1)
-  (public static method fun (x) x)
+  (private static field total-funds 100)
+  (public static method access-private-fields (x)
+          (+ x total-funds))
   (private static method foo () 'foo)
   (private static field total-corporate-funds 2))
 
 (define acc (Account))
-(acc 'n-accounts)
+((acc 'access-private-fields) 10)
